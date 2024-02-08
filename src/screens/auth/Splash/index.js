4689a2e543c3ea8196d96a4c6019e414494ea354
Olaf Olev Alakivi <oalfolev@gmail.com>
2024-02-08 10:02:22 +0200

    WIP start from style

---
+++ src/screens/auth/Splash/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, Image 
+} from "react-native";
+
+const Splash = () => {
+    return(
+        <View>
+            <Image source={require('../../../assets/splash_image.png')} />
+            <Text>You'll Find All you need Here!</Text>
+        </View>
+    )
+}
+export default Splash
