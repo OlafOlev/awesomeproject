095b709a9ff2f9138fb2d57b27b31aef95228c23
Olaf Olev Alakivi <oalfolev@gmail.com>
2024-02-09 11:08:05 +0200

    Create Splash screen

--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -2,17 +2,28 @@ import React from "react";
 import { 
     Text, 
     View, 
-    Image 
+    Image,
+    Pressable
 } from "react-native";
+import Button from "../../../components/Button"
 import { styles } from "./styles";
 
 const Splash = () => {
     return(
         <View style={styles.container}>
             <Image resizeMode="contain" style={styles.image} source={require('../../../assets/splash_image.png')} />
-            <Text style={styles.title}>You'll Find</Text>
-            <Text style={[styles.title, styles.innerTitle]}> All you need</Text>
-            <Text style={styles.title}>Here!</Text>
+
+            <View style={styles.titelContainer}>
+                <Text style={styles.title}>You'll Find</Text>
+                <Text style={[styles.title, styles.innerTitle]}> All you need</Text>
+                <Text style={styles.title}>Here!</Text>
+            </View>
+            
+
+            <Button title="Sign Up"/>
+            <Pressable hitSlop={20}>
+                <Text style={styles.footerText}>Sing In</Text>
+            </Pressable>
         </View>
     )
 }
