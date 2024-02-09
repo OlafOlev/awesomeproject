ed85f0221234604a1765b72daac2bbf351d309da
Olaf Olev Alakivi <oalfolev@gmail.com>
2024-02-09 09:02:30 +0200

    Splash Screen done

--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { Text, View, Image 
+import { 
+    Text, 
+    View, 
+    Image 
 } from "react-native";
+import { styles } from "./styles";
 
 const Splash = () => {
     return(
-        <View>
-            <Image source={require('../../../assets/splash_image.png')} />
-            <Text>You'll Find All you need Here!</Text>
+        <View style={styles.container}>
+            <Image resizeMode="contain" style={styles.image} source={require('../../../assets/splash_image.png')} />
+            <Text style={styles.title}>You'll Find</Text>
+            <Text style={[styles.title, styles.innerTitle]}> All you need</Text>
+            <Text style={styles.title}>Here!</Text>
         </View>
     )
 }
