5fcc1fda4ad0487098f3a290769bffc88e853017
Olaf Olev Alakivi <oalfolev@gmail.com>
2024-02-15 13:24:36 +0200

    7. google done v2

--- App.js
+++ App.js
@@ -6,16 +6,15 @@ import Signup from './src/screens/auth/Signup';
 
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
-const IOS_CLIENT_ID ='367695804296-4d72i99fgrmklsifd16omvf4gj2mj2ec.apps.googleusercontent.com'
-const WEB_CLIENT_ID ='367695804296-mc5pt2ov87akqkc54cd7qadvan7jqr8q.apps.googleusercontent.com'
-const REVERSED_CLIENT_ID ='com.googleusercontent.apps.367695804296-4d72i99fgrmklsifd16omvf4gj2mj2ec'
+import Config from "react-native-config"
+
 const App = () => {
   useEffect(()=> {
     GoogleSignin.configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-      webClientId: WEB_CLIENT_ID, 
+      webClientId: Config.WEB_CLIENT_ID, 
       offlineAccess: true, 
-      iosClientId: IOS_CLIENT_ID,
+      iosClientId: Config.IOS_CLIENT_ID,
     })
   }, [])
   return (
