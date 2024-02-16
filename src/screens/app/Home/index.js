876b6beed8f2a862eae048d3ab46a7e494e75959
Olaf Olev Alakivi <oalfolev@gmail.com>
2024-02-16 12:18:00 +0200

    2. Categories done

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { View,Text} from "react-native";
+import { View,Text, FlatList} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../../components/Header";
 import { styles } from "./styles";
+import { categories} from "../../../data/categories"
+import CategoryBox from "../../../components/CategoryBox";
 
 const Home = () => {
+    const renderCategoryItem = (item, index) => {
+        console.log('item => ', item)
+        return(
+            <CategoryBox title={item?.item?.title} image={item?.item?.image}></CategoryBox> 
+        )
+    }
     return(
         <SafeAreaView>
             <View style={styles.container}>
                 <Header showSearch={true} title="Find All You Need"/>
+                <FlatList showsHorizontalScrollIndicator={false} style={styles.list} horizontal data={categories} renderItem={renderCategoryItem} keyExtractor={(item,index) => String(index)}/>
                 <Text>Home</Text>
             </View>
         </SafeAreaView>
--- src/screens/app/Home/styles.js
+++ src/screens/app/Home/styles.js
@@ -5,6 +5,10 @@ import {colors} from '../../utils/colors'
 
 export const styles = StyleSheet.create({
     container: {
+        padding:24,
     },
+    list:{
+        paddingVertical:24
+    }
 })
 
